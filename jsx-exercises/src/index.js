42c72141dbf39f63156bbd2c63e60fad255b25f3
francesca <francescacoo@gmail.com>
2020-05-24 18:19:37 +0200

    exercises

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom';
 
 
 function myThing(){
-
+  return (
+    <div className='book'>
+      <div className='title'>
+        The title
+      </div>
+      <div className='author'>
+        The author
+      </div>
+      <ul className='stats'>
+        <li className='rating'>
+          5 stars
+        </li>
+        <li className='isbn'>
+          12-3456789-10
+        </li>
+      </ul>
+    </div>
+);
 }
 
 ReactDOM.render(
